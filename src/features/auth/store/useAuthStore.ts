3e437b87a2f7a480881658809e25a80c92a54d9b
Felipe Vieira <flpchapola@hotmail.com>
2025-11-08 01:18:39 -0300

    refactor: remove unused get parameter from auth store
    - Removed unused 'get' parameter from auth store persist callback function

--- src/features/auth/store/useAuthStore.ts
+++ src/features/auth/store/useAuthStore.ts
@@ -37,7 +37,7 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>()(
   persist(
-    (set, get) => ({
+    (set) => ({
       // Initial state
       user: null,
       session: null,
